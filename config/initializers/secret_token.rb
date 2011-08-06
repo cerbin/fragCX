ff5363389c996c96f190f0ece7e70209557bae6f
cerbin <cerbin35@gmail.com>
2011-08-06 20:04:49 +0300

    Основа приложения

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FragCX::Application.config.secret_token = 'd009e62862ef0fd7b28e6e0d0153928cd33a1a0982bf601898801911a0b17d6d6cdfcf371a8c57cb4c09d50771af1c001c020692c982275235b9cb12320c9f45'
